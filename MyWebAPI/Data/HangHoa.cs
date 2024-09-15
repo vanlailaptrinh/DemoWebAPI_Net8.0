21d20f35be36dd0252623a2647336653192611ca
VanTran <vantran15122004@gmail.com>
2024-09-15 22:15:53 +0700

    Upload WorkingDB

---
+++ MyWebAPI/Data/HangHoa.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyWebAPI.Data
+{
+
+    [Table("HangHoa")]
+    public class HangHoa
+    {
+        [Key]
+        public Guid MaHh { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public String TenHh { get; set; }
+
+        public String MoTa { get; set; }
+
+
+        [Range(0, double.MaxValue)]
+        public double DonGia { get; set; }
+
+        public byte GiamGia { get; set; }
+
+        public int? MaLoai { get; set; }
+        [ForeignKey("MaLoai")]
+        public Loai Loai { get; set; }
+
+
+
+    }
+
+}
---
+++ MyWebAPI/Data/Loai.cs
@@ -0,0 +1,20 @@
+﻿    using System.ComponentModel.DataAnnotations;
+
+    namespace MyWebAPI.Data
+    {
+        public class Loai
+        {
+            [Key]
+            public int MaLoai { get; set; }
+
+
+            [Required]
+            [MaxLength(50)]
+            public String TenLoai { get; set; }
+
+            public virtual ICollection<HangHoa> HangHoas { get; set; }
+
+    
+
+        }
+    }
